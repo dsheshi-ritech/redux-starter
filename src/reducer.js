270649fc24963e9d03f6cedfad08e5fb9a174da6
dejvis.sheshi <dsheshi@ritech.co>
2020-05-28 19:41:56 +0200

    master - Initial commit

--- src/reducer.js
+++ src/reducer.js
@@ -3,21 +3,24 @@ let lastId = 0;
 
 export default function reducer(state = [], action) {
   //or with switch case
-  if (action.type === actions.BUG_ADDED)
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  else if (action.type === actions.BUG_REMOVED)
-    return state.filter((bug) => bug.id !== action.payload.id);
-  else if (action.type === actions.BUG_RESOLVED)
-    return state.map(
-      ({ id, resolved }) =>
-        id !== action.payload.id ? bug : { ...bug, resolved: true } // update in an immutable way
-    );
-  return state;
+  switch (action.type) {
+    case actions.BUG_ADDED:
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case actions.BUG_REMOVED:
+      return state.filter((bug) => bug.id !== action.payload.id);
+    case actions.BUG_RESOLVED:
+      return state.map(
+        ({ id, resolved }) =>
+          id !== action.payload.id ? bug : { ...bug, resolved: true } // update in an immutable way
+      );
+    default:
+      return state;
+  }
 }
